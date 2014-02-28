b57c4196e9af365ae6bd9d5aa6068b6411aee083
Tim Cooijmans <timcooijmans@gmail.com>
2014-02-28 14:49:44 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='SimpleFramework',
-      version='1.2',
+      version='1.2.1',
       description='Simple Web Framework by Simplendi',
       author='Simplendi',
       author_email='info@simplendi.com',
